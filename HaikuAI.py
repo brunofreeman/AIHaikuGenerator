154653cb6029852de31f253fe7f66b1ecf2ed759
brunofreeman <brunofreeman21@gmail.com>
2019-03-16 10:52:09 -0400

    deleting old file

--- HaikuAI.py
+++
@@ -1,9 +0,0 @@
-#https://www.youtube.com/watch?v=ZGU5kIG7b2I
-#dependencies
-
-import numpy as np #vectorization
-import random #generating text
-import tensorflow #ML
-import datetime #clock training time
-
-text = open('').read()
