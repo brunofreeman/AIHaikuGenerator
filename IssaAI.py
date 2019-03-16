91f93a424a60332c656ac6dc743213d6f6e4ac53
brunofreeman <brunofreeman21@gmail.com>
2019-03-16 10:50:10 -0400

    renamed file

---
+++ IssaAI.py
@@ -0,0 +1,9 @@
+#https://www.youtube.com/watch?v=ZGU5kIG7b2I
+#dependencies
+
+import numpy as np #vectorization
+import random #generating text
+import tensorflow #ML
+import datetime #clock training time
+
+text = open('').read()
