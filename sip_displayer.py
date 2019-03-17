15918db6dfa1274623eabbb36ce4ba947af9a2c2
brunofreeman <brunofreeman21@gmail.com>
2019-03-17 13:41:57 -0400

    added reset button

--- sip_displayer.py
+++ sip_displayer.py
@@ -5,7 +5,14 @@ import tkinter.font
 def change_SIP():
 	sip_text.set("")
 	window.update()
-	sip_text.set(generate_SIP(""))\
+	sip_text.set(generate_SIP(""))
+
+def reset_SIP():
+	sip_text.set("")
+	window.update()
+	sip_text.set(default_sip)
+
+default_sip = "authored by computer...\nshort Issa poems for you\nnow, click the button"
 
 window = Tk()
 window.title("Issa Generator")
@@ -15,7 +22,7 @@ window.geometry(str(window_size) + "x" + str(window_size))
 #window.resizable(width=False, height=False)
 
 sip_text = StringVar()
-sip_text.set("authored by computer...\nshort Issa poems for you\nnow, click the button")
+sip_text.set(default_sip)
 sip_font = tkinter.font.Font(family="Helvetica", size=36, weight="bold")
 sip_label = Label(window, textvariable=sip_text, font=sip_font, bg="black", fg="white")
 sip_label.place(relx=0.5, rely=0.4, anchor="c")
@@ -27,5 +34,8 @@ generate_button.place(relx=0.5, rely=0.8, anchor="c")
 small_font = tkinter.font.Font(family="Helvetica", size=12)
 sub_label = Label(window, text="(Short Issa Poem)", font=small_font, bg="black", fg="white")
 sub_label.place(relx=0.5, rely=0.85, anchor="c")
+
+generate_button = Button(window, text="Reset SIP", font=small_font, command=reset_SIP)
+generate_button.place(relx=0.5, rely=0.9, anchor="c")
 
 mainloop()
