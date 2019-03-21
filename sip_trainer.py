972552e69025971eb45aba7051192ffeee3d5cf4
brunofreeman <33364693+brunofreeman@users.noreply.github.com>
2019-03-20 22:06:34 -0400

    cleaning training data

--- sip_trainer.py
+++ sip_trainer.py
@@ -1,7 +1,7 @@
 from sip_lstm import train_SIP
 
-max_steps = 10501
-log_every = 50
-save_every = 500
+max_steps = 501
+log_every = 10
+save_every = 100
 
 train_lstm(max_steps, log_every, save_every)
